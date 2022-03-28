bf03172deb554e39b306f1882715470918ed0425
macbook <khaitruong1301@gmail.com>
2022-03-28 21:26:10 +0700

    buoi2_style_state_map

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import CardRFC from './Components/CardRFC';
 import BTLayout from './Components/BTLayout/BTLayout';
 import DataBinding from './DataBinding/DataBinding';
 import HandleEvent from './HandleEvent/HandleEvent';
+import StateDemo from './StateDemo/StateDemo';
+import StyleComponent from './StyleComponent/StyleComponent';
+import RenderArray from './RenderArray/RenderArray';
 
 function App() {
   return (
     <div className="App">
       {/* <BTLayout /> */}
       {/* <DataBinding /> */}
-      <HandleEvent />
+      {/* <HandleEvent /> */}
+      {/* <StateDemo /> */}
+      {/* <StyleComponent /> */}
+      {/* <p className='text-red'>app component</p> */}
+      <RenderArray />
     </div>
   );
 }
